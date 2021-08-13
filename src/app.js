7b93c2903eb57d93179569af707fdd38dd1610ca
Dennis Morello <dennis.morello@contentwise.com>
2021-08-13 16:23:46 +0200

    refactor(case): renamed app.js to lowercase

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+
+import SolarSystem from "./components/solar-system";
+
+const Scene = () => (
+  <>
+    <pointLight color={0xffffff} intensity={3} />
+    <SolarSystem />
+  </>
+);
+
+const App = () => (
+  <Canvas>
+    <Suspense fallback={null}>
+      <Scene />
+      <PerspectiveCamera makeDefault fov={40} position={[0, 50, 0]} />
+      <OrbitControls />
+    </Suspense>
+  </Canvas>
+);
+
+export default App;
